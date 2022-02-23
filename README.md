7a28e87f009d6d73a865671a70c206ea72cc2580
Mikhail Fedorov <m.fedorov@admitad.com>
2022-02-23 05:02:37 +0300

    Fix review remarks

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # db-hack-devman
 
-Script for hack school databease
+Script for hack school database
 
 ## Usage
 
 There are three functions in file:
 
-- fix_marks: get student name and replace all 2 and 3 marks to 5
-- remove_chastisements: get student name and remove all chastisements
-- create_commendation: get student name and subject and add commendation with random text
+To fix marks of Иван Иванов from 2 or 3 to 5
+```python
+fix_marks('Иван Иванов')
+```  
+To remove chastisements of Иван Иванов
+```python
+remove_chastisements('Иван Иванов')
+```
+To create commendation of Иван Иванов for subject Музыка 
+```python
+create_commendation('Иван Иванов', 'Музыка')
+```
 
 
 # Project Goals
--- scripts.py
+++ scripts.py
@@ -1,87 +1,82 @@
 import random
 
 from datacenter.models import Chastisement, Commendation, Lesson, Mark, Schoolkid, Subject
+from typing import List, Optional
 
 
-def fix_marks(schoolkid_full_name: str):
+def get_schoolkid(schoolkid_full_name: str) -> Optional[Schoolkid]:
     schoolkids = Schoolkid.objects.filter(full_name__contains=schoolkid_full_name)
-    if len(schoolkids) > 1:
-        print('There is more than one student with this name')
+    if len(schoolkids) != 1:
+        print(f'There is {"less" if len(schoolkids) < 1 else "more"} than one student with this name')
         return
-    if not schoolkids:
-        print('There is more than one student with this name')
-        return
-    schoolkid = schoolkids[0]
-    Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=5)
+    return schoolkids.get()
 
 
-def remove_chastisements(schoolkid_full_name: str):
-    schoolkids = Schoolkid.objects.filter(full_name__contains=schoolkid_full_name)
-    if len(schoolkids) > 1:
-        print('There is more than one student with this name')
-        return
-    if not schoolkids:
-        print('There is more than one student with this name')
-        return
-    schoolkid = schoolkids[0]
-    Chastisement.objects.filter(schoolkid=schoolkid).delete()
+def fix_marks(schoolkid_full_name: str) -> None:
+    schoolkid = get_schoolkid(schoolkid_full_name)
+    if schoolkid:
+        Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=5)
 
 
-commendations = [
-    'Молодец!',
-    'Отлично!',
-    'Хорошо!',
-    'Гораздо лучше, чем я ожидал!',
-    'Ты меня приятно удивил!',
-    'Великолепно!',
-    'Прекрасно!',
-    'Ты меня очень обрадовал!',
-    'Именно этого я давно ждал от тебя!',
-    'Сказано здорово – просто и ясно!',
-    'Ты, как всегда, точен!',
-    'Очень хороший ответ!',
-    'Талантливо!',
-    'Ты сегодня прыгнул выше головы!',
-    'Я поражен!',
-    'Уже существенно лучше!',
-    'Потрясающе!',
-    'Замечательно!',
-    'Прекрасное начало!',
-    'Так держать!',
-    'Ты на верном пути!',
-    'Здорово!',
-    'Это как раз то, что нужно!',
-    'Я тобой горжусь!',
-    'С каждым разом у тебя получается всё лучше!',
-    'Мы с тобой не зря поработали!',
-    'Я вижу, как ты стараешься!',
-    'Ты растешь над собой!',
-    'Ты многое сделал, я это вижу!',
-    'Теперь у тебя точно все получится!']
+def remove_chastisements(schoolkid_full_name: str) -> None:
+    schoolkid = get_schoolkid(schoolkid_full_name)
+    if schoolkid:
+        Chastisement.objects.filter(schoolkid=schoolkid).delete()
 
 
-def create_commendation(schoolkid_full_name, subject_title: str):
-    schoolkids = Schoolkid.objects.filter(full_name__contains=schoolkid_full_name)
-    if len(schoolkids) > 1:
-        print('There is more than one student with this name')
+def get_commendations() -> List[str]:
+    return [
+        'Молодец!',
+        'Отлично!',
+        'Хорошо!',
+        'Гораздо лучше, чем я ожидал!',
+        'Ты меня приятно удивил!',
+        'Великолепно!',
+        'Прекрасно!',
+        'Ты меня очень обрадовал!',
+        'Именно этого я давно ждал от тебя!',
+        'Сказано здорово – просто и ясно!',
+        'Ты, как всегда, точен!',
+        'Очень хороший ответ!',
+        'Талантливо!',
+        'Ты сегодня прыгнул выше головы!',
+        'Я поражен!',
+        'Уже существенно лучше!',
+        'Потрясающе!',
+        'Замечательно!',
+        'Прекрасное начало!',
+        'Так держать!',
+        'Ты на верном пути!',
+        'Здорово!',
+        'Это как раз то, что нужно!',
+        'Я тобой горжусь!',
+        'С каждым разом у тебя получается всё лучше!',
+        'Мы с тобой не зря поработали!',
+        'Я вижу, как ты стараешься!',
+        'Ты растешь над собой!',
+        'Ты многое сделал, я это вижу!',
+        'Теперь у тебя точно все получится!']
+
+
+def create_commendation(schoolkid_full_name: str, subject_title: str) -> None:
+    schoolkid = get_schoolkid(schoolkid_full_name)
+    if not schoolkid:
         return
-    if not schoolkids:
-        print('There is more than one student with this name')
-        return
-    schoolkid = schoolkids[0]
-    subject = Subject.objects.filter(title=subject_title, year_of_study=schoolkid.year_of_study)[0]
+    subject = Subject.objects.filter(title=subject_title, year_of_study=schoolkid.year_of_study).get()
     lessons = Lesson.objects.filter(
         year_of_study=schoolkid.year_of_study,
         group_letter=schoolkid.group_letter,
         subject=subject,
     ).order_by('-date')
-    last_lesson = lessons.first()
+    if not lessons:
+        print('There is no lessons with this student')
+        return
+    last_lesson = lessons.get()
     commendation = Commendation(
-        text=random.choice(commendations),
+        text=random.choice(get_commendations()),
         created=last_lesson.date,
         schoolkid=schoolkid,
         subject=subject,
         teacher=last_lesson.teacher,
     )
-    print(commendation)
     commendation.save()
